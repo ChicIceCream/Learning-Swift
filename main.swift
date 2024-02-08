c675c080595b457f36c322eaa47e110f5d420524
ChicIceCream <abhivyaktbhati1102@gmail.com>
2024-02-08 21:48:03 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,11 @@
+// Hello, World! program in Swift
+
+
+print("Hello, World!")
+var num = 69
+print(num)
+
+let label = "The width is "
+let width = 94
+print("Num \(num) plus width \(width) is equal to : \(num + width)")
+let cat = "🐱"; print(cat)
